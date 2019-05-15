4b43b5954c221ca392176d3b2887c4340d4be51b
yo-C-ta <45458949+yo-C-ta@users.noreply.github.com>
2019-05-15 23:43:30 +0900

    Add feature to insert cursor to any number of rows

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,57 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log(
+        'Congratulations, your extension "insert-multiple-rows" is now active!'
+    );
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "insert-multiple-rows" is now active!');
+    /*------------------------------------------------------------------------------------------*/
+    /*  Insert cursor support function                                                          */
+    /*------------------------------------------------------------------------------------------*/
+    const insCursorToOtherRows = (
+        editor: vscode.TextEditor,
+        sels: vscode.Selection[],
+        insRows: () => void
+    ) => {
+        if (sels.length === 1) {
+            vscode.window
+                .showInputBox({
+                    value: '1',
+                    prompt: 'How many rows?',
+                    validateInput: param => {
+                        const regex = /^[0-9]+$/;
+                        return regex.test(param) ? '' : 'input: decimal number';
+                    },
+                })
+                .then(value => {
+                    if (value === undefined || editor === undefined) {
+                        return;
+                    }
+                    const linenum = parseInt(value, 10);
+                    const curline = sels[0].start.line + 1;
+                    for (
+                        let lnum = curline;
+                        lnum < curline + linenum - 1 &&
+                        lnum < editor.document.lineCount;
+                        lnum++
+                    ) {
+                        const cnum = editor.document.lineAt(lnum).text.length;
+                        const position = new vscode.Position(lnum, cnum);
+                        sels.push(new vscode.Selection(position, position));
+                    }
+                    insRows();
+                });
+        } else {
+            insRows();
+        }
+    };
 
     /*------------------------------------------------------------------------------------------*/
     /*  Insert decimal to multiple rows                                                         */
     /*------------------------------------------------------------------------------------------*/
-    let insDecMulrowsCmd = vscode.commands.registerCommand(
+    const insDecMulrowsCmd = vscode.commands.registerCommand(
         'extension.insDecMulrows',
         () => {
             vscode.window
@@ -22,26 +59,43 @@ export function activate(context: vscode.ExtensionContext) {
                     value: '1',
                     prompt: 'Insert dec from:',
                     validateInput: param => {
-                        var regex = /^[0-9]+$/;
+                        const regex = /^[0-9]+$/;
                         return regex.test(param) ? '' : 'input: decimal number';
-                    }
+                    },
                 })
                 .then(value => {
-                    var editor = vscode.window.activeTextEditor;
-                    if (value === undefined || editor === undefined) { return; }
-                    var sels = editor.selections;
-                    var num = parseInt(value, 10);
-                    var digit = Math.trunc(0.435 * Math.log(sels.length + num - 1)) + 1;
-                    var padconf = vscode.workspace.getConfiguration()
-                        .get('insertDecimalToMultipleRows.paddingChar');
-                    if (padconf === undefined || typeof padconf !== 'string') { return; }
-                    var padding = padconf.charAt(0).repeat(10);
-                    editor.edit(edit => {
-                        sels.forEach(select => {
-                            var text = (padding + (num++).toString()).slice(-digit);
-                            edit.insert(select.start, text);
+                    const editor = vscode.window.activeTextEditor;
+                    if (value === undefined || editor === undefined) {
+                        return;
+                    }
+                    const sels = editor.selections;
+                    let num = parseInt(value, 10);
+
+                    const insertDec = () => {
+                        const padconf = vscode.workspace
+                            .getConfiguration()
+                            .get('insertDecimalToMultipleRows.paddingChar');
+                        if (
+                            padconf === undefined ||
+                            typeof padconf !== 'string'
+                        ) {
+                            return;
+                        }
+                        const padding = padconf.charAt(0).repeat(10);
+                        const digit =
+                            Math.trunc(
+                                0.435 * Math.log(sels.length + num - 1)
+                            ) + 1;
+                        editor.edit(edit => {
+                            sels.forEach(select => {
+                                const text = (
+                                    padding + (num++).toString()
+                                ).slice(-digit);
+                                edit.insert(select.start, text);
+                            });
                         });
-                    });
+                    };
+                    insCursorToOtherRows(editor, sels, insertDec);
                 });
         }
     );
@@ -49,7 +103,7 @@ export function activate(context: vscode.ExtensionContext) {
     /*------------------------------------------------------------------------------------------*/
     /*  Insert bitfield to multiple rows                                                        */
     /*------------------------------------------------------------------------------------------*/
-    let insBitFldMulrowsCmd = vscode.commands.registerCommand(
+    const insBitFldMulrowsCmd = vscode.commands.registerCommand(
         'extension.insBitFldMulrows',
         () => {
             vscode.window
@@ -58,26 +112,43 @@ export function activate(context: vscode.ExtensionContext) {
                     valueSelection: [2, 4],
                     prompt: 'Insert bitfield from:',
                     validateInput: param => {
-                        let regex = /^0x0{0,7}[1248]0{0,7}$/;
-                        return regex.test(param) ? '' : 'input: bitfield (\'0x\' format)';
-                    }
+                        const regex = /^0x0{0,7}[1248]0{0,7}$/;
+                        return regex.test(param)
+                            ? ''
+                            : "input: bitfield ('0x' format)";
+                    },
                 })
                 .then(value => {
-                    var editor = vscode.window.activeTextEditor;
-                    if (value === undefined || editor === undefined) { return; }
-                    var shift = Math.trunc(1.443 * Math.log(parseInt(value, 16)));
-                    var sels = editor.selections;
-                    var digit = 8;
-                    if (sels.length + shift <= 8) { digit = 2; }
-                    else if (sels.length + shift <= 16) { digit = 4; }
-                    editor.edit(edit => {
-                        sels.forEach(select => {
-                            var text = (1 << shift).toString(16).toUpperCase();
-                            text = ('0'.repeat(8) + text).slice(-digit);
-                            edit.insert(select.start, '0x' + text);
-                            shift++;
+                    const editor = vscode.window.activeTextEditor;
+                    if (value === undefined || editor === undefined) {
+                        return;
+                    }
+                    const sels = editor.selections;
+                    let shift = Math.trunc(
+                        1.443 * Math.log(parseInt(value, 16))
+                    );
+
+                    const insertBf = () => {
+                        let digit = 8;
+                        if (sels.length + shift <= 8) {
+                            digit = 2;
+                        } else if (sels.length + shift <= 16) {
+                            digit = 4;
+                        }
+                        editor.edit(edit => {
+                            sels.forEach(select => {
+                                const text = (1 << shift)
+                                    .toString(16)
+                                    .toUpperCase();
+                                edit.insert(
+                                    select.start,
+                                    '0x' + ('0'.repeat(8) + text).slice(-digit)
+                                );
+                                shift++;
+                            });
                         });
-                    });
+                    };
+                    insCursorToOtherRows(editor, sels, insertBf);
                 });
         }
     );
@@ -85,7 +156,7 @@ export function activate(context: vscode.ExtensionContext) {
     /*------------------------------------------------------------------------------------------*/
     /*  Insert charcter to multiple rows                                                        */
     /*------------------------------------------------------------------------------------------*/
-    let insCharMulrowsCmd = vscode.commands.registerCommand(
+    const insCharMulrowsCmd = vscode.commands.registerCommand(
         'extension.insCharMulrows',
         () => {
             vscode.window
@@ -93,33 +164,53 @@ export function activate(context: vscode.ExtensionContext) {
                     value: 'A',
                     prompt: 'Insert char from:',
                     validateInput: param => {
-                        var regex = /^[a-zA-Z]+$/;
+                        const regex = /^[a-zA-Z]+$/;
                         return regex.test(param) ? '' : 'input: charactor';
-                    }
+                    },
                 })
                 .then(value => {
-                    var editor = vscode.window.activeTextEditor;
-                    if (value === undefined || editor === undefined) { return; }
-                    var len = value.split('').length;
-                    var num = value.split('').map(elm => {
-                        var char = elm.charCodeAt(0);
-                        char -= char <= 91 ? 65 : 97;
-                        return char * Math.pow(26, --len);
-                    }).reduce((prv, current) => {
-                        return prv + current;
-                    });
-                    var sels = editor.selections;
-                    var digit = Math.trunc(0.307 * Math.log(sels.length + num - 1)) + 1;
-                    editor.edit(edit => {
-                        sels.forEach(select => {
-                            var text = ('0'.repeat(10) + (num++).toString(26)).slice(-digit);
-                            edit.insert(select.start, text.split('').map(elm => {
-                                return String.fromCharCode(
-                                    parseInt(elm, 26) + 65
-                                ).toString();
-                            }).join(''));
+                    const editor = vscode.window.activeTextEditor;
+                    if (value === undefined || editor === undefined) {
+                        return;
+                    }
+                    let len = value.split('').length;
+                    let num = value
+                        .split('')
+                        .map(elm => {
+                            let char = elm.charCodeAt(0);
+                            char -= char <= 91 ? 65 : 97;
+                            return char * Math.pow(26, --len);
+                        })
+                        .reduce((prv, current) => {
+                            return prv + current;
                         });
-                    });
+                    const sels = editor.selections;
+
+                    const insertChar = () => {
+                        const digit =
+                            Math.trunc(
+                                0.307 * Math.log(sels.length + num - 1)
+                            ) + 1;
+                        editor.edit(edit => {
+                            sels.forEach(select => {
+                                const text = (
+                                    '0'.repeat(10) + (num++).toString(26)
+                                ).slice(-digit);
+                                edit.insert(
+                                    select.start,
+                                    text
+                                        .split('')
+                                        .map(elm => {
+                                            return String.fromCharCode(
+                                                parseInt(elm, 26) + 65
+                                            ).toString();
+                                        })
+                                        .join('')
+                                );
+                            });
+                        });
+                    };
+                    insCursorToOtherRows(editor, sels, insertChar);
                 });
         }
     );
@@ -131,6 +222,4 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(insCharMulrowsCmd);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
